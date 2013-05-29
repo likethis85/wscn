343b4d506f95d6551b4ea24696a96e4e23c05cc7
AlloVince <allo.vince@gmail.com>
2013-05-29 10:34:04 +0800

    ie image fix

--- sites/all/themes/wallstcn/templates/views-view-fields--x-click.tpl.php
+++ sites/all/themes/wallstcn/templates/views-view-fields--x-click.tpl.php
@@ -19,7 +19,7 @@ if($wscn['x_click_rendered']) return;
             <div class="media">
                 <?if($item->file_managed_field_data_upload_uri || $item->field_field_image_1):?>
                 <a class="pull-left" href="<?=url('node/'. $item->nid);?>">
-                    <img alt="" width="80" class="" src="<?=wscn_image_url($item);?>">
+                    <img alt="" class="" src="<?=wscn_image_url($item);?>">
                 </a>
                 <?endif?>
                 <div class="media-body">
