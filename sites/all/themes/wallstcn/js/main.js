96df14a10864f6cf6b21f55b1dd08d3aef6773db
AlloVince <allo.vince@gmail.com>
2013-06-21 14:45:57 +0800

    to 500

--- sites/all/themes/wallstcn/js/main.js
+++ sites/all/themes/wallstcn/js/main.js
@@ -93,7 +93,7 @@
         //图片延迟加载
         $("img.lazy").show().lazyload({
             effect       : "fadeIn",
-            threshold : 200,
+            threshold : 500,
             load : function() {
                 var img = $(this);
                 if(img.parent().hasClass('news-img-wrap') && img.height() < img.parent().height()){
