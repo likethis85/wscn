c2a45a0e28a6ddbf01bb4004ae0c04882e5c419f
AlloVince <allo.vince@gmail.com>
2013-06-24 16:23:51 +0800

    ad changes

--- sites/all/themes/wallstcn/templates/region--index_content.tpl.php
+++ sites/all/themes/wallstcn/templates/region--index_content.tpl.php
@@ -9,19 +9,18 @@
             <a target="_blank" href="http://nordfx-chinese.com/promo/bonus_deposit.html?id=730026"><img src="/sites/all/themes/wallstcn/ads/ad_nordfx.gif" alt=""></a>
         </div>
         <div class="span4">
-            <!-- G-wallstreetcn.com/ufxmarkets -->
             <script type="text/javascript">
-                var ord = window.ord || Math.floor(Math.random() * 1e16);
-                document.write('<script type="text/javascript" src="http://ad.doubleclick.net/N6105/adj/G-wallstreetcn.com/ufxmarkets;sz=230x60;ord=' + ord + '?"><\/script>');
+              var ord = window.ord || Math.floor(Math.random() * 1e16);
+              document.write('<script type="text/javascript" src="http://ad.doubleclick.net/N6105/adj/G-wallstreetcn.com/hantecfx;sz=230x60;ord=' + ord + '?"><\/script>');
             </script>
             <noscript>
-                <a href="http://ad.doubleclick.net/N6105/jump/G-wallstreetcn.com/ufxmarkets;sz=230x60;ord=[timestamp]?">
-                    <img src="http://ad.doubleclick.net/N6105/ad/G-wallstreetcn.com/ufxmarkets;sz=230x60;ord=[timestamp]?" width="230" height="60" />
-                </a>
+            <a href="http://ad.doubleclick.net/N6105/jump/G-wallstreetcn.com/hantecfx;sz=230x60;ord=[timestamp]?">
+            <img src="http://ad.doubleclick.net/N6105/ad/G-wallstreetcn.com/hantecfx;sz=230x60;ord=[timestamp]?" width="230" height="60" />
+            </a>
             </noscript>
         </div>
         <div class="span4 right-align">
-            <img src="/sites/all/themes/wallstcn/ads/ad_weixin.gif" alt="">
+            <img src="/sites/all/themes/wallstcn/ads/ad_weixin.gif" alt="" class="weixin">
             <?if(0):?>
             <a target="_blank" href="http://www.maslink.com/sh/lunbotu/bk/index.html"><img src="http://pic.yupoo.com/panzhiyao/CPw2EA0t/RKhAe.gif" alt=""></a>
             <?endif?>
