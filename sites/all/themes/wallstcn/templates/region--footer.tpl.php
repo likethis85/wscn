20640031e6cd196351a66f411f7b94fae7dc0ff4
AlloVince <allo.vince@gmail.com>
2013-08-29 15:59:22 +0800

    add link

--- sites/all/themes/wallstcn/templates/region--footer.tpl.php
+++ sites/all/themes/wallstcn/templates/region--footer.tpl.php
@@ -53,6 +53,7 @@
 <a href="http://finance.591hx.com/" target="_blank">华讯财经</a> | 
 <a href="http://www.eastmoney.com/" target="_blank">东方财富网</a> | 
 <a href="http://www.jrj.com.cn/" target="_blank">金融界</a> | 
+<a href="http://www.stockstar.com/" target="_blank">证券之星</a> | 
 <a href="http://www.p5w.net/news/index.htm" target="_blank">全景财经</a> | 
 <a href="http://finance.kankanews.com/" target="_blank">看看新闻网</a> | 
 <a href="http://www.financialnews.com.cn/" target="_blank">中国金融新闻网</a> | 
