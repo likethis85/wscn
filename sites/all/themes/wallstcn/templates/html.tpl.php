8e8ae6e4e56163253669f1939853e788904f9a39
AlloVince <allo.vince@gmail.com>
2013-09-01 15:45:23 +0800

    ad changed

--- sites/all/themes/wallstcn/templates/html.tpl.php
+++ sites/all/themes/wallstcn/templates/html.tpl.php
@@ -31,5 +31,11 @@
     <?php print $page_top; ?>
     <?php print $page; ?>
     <?php print $page_bottom; ?>
+<script type="text/javascript">//<![CDATA[
+ac_as_id = 2107398;
+ac_click_track_url = "";ac_format = 0;ac_mode = 1;
+ac_width = 280;ac_height = 210;
+//]]></script>
+<script type="text/javascript" src="http://static.acs86.com/g.js"></script>
 </body>
 </html>
