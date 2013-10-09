1024cc777cf85efe0fdb2acc2db28a469db4e456
han1015578159 <77620429@qq.com>
2013-10-09 12:43:05 +0800

    alexa

--- sites/all/themes/wallstcn/templates/html.tpl.php
+++ sites/all/themes/wallstcn/templates/html.tpl.php
@@ -41,5 +41,14 @@ ac_click_track_url = "";ac_format = 0;ac_mode = 1;
 ac_width = 280;ac_height = 210;
 //]]></script>
 <script type="text/javascript" src="http://static.acs86.com/g.js"></script>
+
+<!-- Start Alexa Certify Javascript -->
+<script type="text/javascript">
+_atrk_opts = { atrk_acct:"+YhCf1a8Md00aX", domain:"wallstreetcn.com",dynamic: true};
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+</script>
+<noscript><img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=+YhCf1a8Md00aX" style="display:none" height="1" width="1" alt="" /></noscript>
+<!-- End Alexa Certify Javascript -->
+
 </body>
 </html>
