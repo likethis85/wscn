3bc3a8a5cf218c1f5da81a551d99f195c3ce2e57
han1015578159 <77620429@qq.com>
2013-11-20 15:49:41 +0800

    ads

--- sites/all/themes/wallstcn/templates/node.tpl.php
+++ sites/all/themes/wallstcn/templates/node.tpl.php
@@ -150,17 +150,17 @@ drupal_set_title($page_title, PASS_THROUGH);?>
 <?if(variable_get('site_ad')):?>
 <div class="random-ad" style="margin-bottom:0;">
     <div class="carousel-inner">
-        <div class="active item"  data-probability="60">
+        <div class="active item"  data-probability="50">
             <div style="height:90px;margin:0 auto;">
                 <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" height="90" width="728"><param name="quality" value="high" /><param name="movie" value="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/live_top_ad_1_MT4.swf" /><embed height="90" pluginspage="http://www.macromedia.com/go/getflashplayer" quality="high" src="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/live_top_ad_1_MT4.swf" type="application/x-shockwave-flash" width="728" wmode="transparent"></embed></object>
             </div>
         </div>
-        <div class="item"  data-probability="20">
+        <div class="item"  data-probability="25">
             <div style="height:90px;margin:0 auto;">
                 <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" height="90" width="728"><param name="quality" value="high" /><param name="movie" value="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/live_top_ad_2_svsfx.swf" /><embed height="90" pluginspage="http://www.macromedia.com/go/getflashplayer" quality="high" src="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/live_top_ad_2_svsfx.swf" type="application/x-shockwave-flash" width="728" wmode="transparent"></embed></object>
             </div>
         </div>
-        <div class="item"   data-probability="20">
+        <div class="item"   data-probability="25">
             <div style="margin:0 auto;">
                 <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" height="90" width="728"><param name="quality" value="high" /><param name="movie" value="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/index_top_ad_2_GB513.swf" /><embed height="90" pluginspage="http://www.macromedia.com/go/getflashplayer" quality="high" src="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/index_top_ad_2_GB513.swf" type="application/x-shockwave-flash" width="728"></embed></object>
             </div>
