e6006fefd454013dac2101ba51f8d4849c9d7c14
han1015578159 <77620429@qq.com>
2013-11-29 13:33:00 +0800

    sed

--- sites/all/themes/wallstcnlive/templates/html.tpl.php
+++ sites/all/themes/wallstcnlive/templates/html.tpl.php
@@ -6,9 +6,21 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <?if ($node = menu_get_object()):?>
+    <title><?=$head_title; ?></title>
+    <?if (isset($node->taxonomy_vocabulary_3['und'])):?>
+    <meta name="keywords" content="<?foreach($node->taxonomy_vocabulary_3['und'] as $tag):?><?=$tag['taxonomy_term']->name?>,<?endforeach?>" />
+    <?endif?>
+    <?else:?>
+    <title>华尔街见闻_中国最专业金融资讯平台_外汇期货_大宗商品_贵金属_黄金_股市_行情</title>
+    <meta name="keywords" content="直播,外汇,期货,金融,黄金,白银,债券,股市,投资,财经日历,银行,新闻,美元,欧元,日元,人民币," />
+    <meta name="description" content="华尔街新闻——中国最专业的金融资讯平台；直播外汇、期货、黄金、债券、证券等金融领域的实时新闻。华人投资者可以在这里获得最快速、最精准、最深入的全球财经资讯和市场行情。" />
+    <?endif?>
+    <?if (0):?>
     <title><?=$head_title; ?></title>
     <?if($is_front):?>
     <meta name="description" content="华尔街见闻隶属上海阿牛信息科技有限公司，是一个专业的的全球金融资讯中文平台 商，为全球金融市场投资者和从业者提供经济和金融市场信息">
+    <?endif?>
     <?endif?>
     <meta name="viewport" content="width=device-width">
     <?=$head?>
