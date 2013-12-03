092f03e4b0cde590f57fe5ab29ad3c16a0109347
han1015578159 <77620429@qq.com>
2013-12-03 13:43:29 +0800

    update

--- sites/all/modules/entityreference/plugins/selection/EntityReference_SelectionHandler_Generic.class.php
+++ sites/all/modules/entityreference/plugins/selection/EntityReference_SelectionHandler_Generic.class.php
@@ -304,7 +304,8 @@ class EntityReference_SelectionHandler_Generic implements EntityReference_Select
    * Implements EntityReferenceHandler::getLabel().
    */
   public function getLabel($entity) {
-    return entity_label($this->field['settings']['target_type'], $entity);
+    $target_type = $this->field['settings']['target_type'];
+    return entity_access('view', $target_type, $entity) ? entity_label($target_type, $entity) : t('- Restricted access -');
   }
 
   /**
@@ -340,7 +341,7 @@ class EntityReference_SelectionHandler_Generic implements EntityReference_Select
     $entity_info = entity_get_info($target_type);
     $id = $entity_info['entity keys']['id'];
     // Return the alias of the table.
-    return $query->innerJoin($target_type, NULL, "$target_type.$id = $alias.entity_id");
+    return $query->innerJoin($target_type, NULL, "%alias.$id = $alias.entity_id");
   }
 }
 
