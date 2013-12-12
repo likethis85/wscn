910f5a14339d25a1b3395fd1f8c0fb6dde629dd9
sunshine <sunsheng_it@163.com>
2013-12-12 11:19:45 +0800

    top-news 底部对齐

--- sites/all/themes/wallstcn/templates/views-view-fields--x-topnews.tpl.php
+++ sites/all/themes/wallstcn/templates/views-view-fields--x-topnews.tpl.php
@@ -17,7 +17,7 @@ foreach($items as $key => $news) {
 }
 ?>
 
-<div id="top-news" class="<?if(!$item->field_field_related):?>no-related<?endif?> <?=$classes;?>" <?=$attributes;?>>
+<div id="top-news" class="align-bottom <?if(!$item->field_field_related):?>no-related<?endif?> <?=$classes;?>" <?=$attributes;?>>
     <div class="row-fluid">
         <div class="span7">
             <div class="top-news-entry">
