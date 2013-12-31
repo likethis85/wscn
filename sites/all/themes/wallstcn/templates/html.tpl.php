381f6c6efd524e63eb151ab6a92eeba62f86e318
han1015578159 <77620429@qq.com>
2013-12-31 15:21:11 +0800

    js

--- sites/all/themes/wallstcn/templates/html.tpl.php
+++ sites/all/themes/wallstcn/templates/html.tpl.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <?
 $styles = str_ireplace('http://wallstreetcn.com', 'http://img.wallstreetcn.com', $styles);
-$styles = str_ireplace('http://live.wallstreetcn.com', 'http://img.wallstreetcn.com', $styles);
 $scripts = str_ireplace('http://wallstreetcn.com', 'http://img.wallstreetcn.com', $scripts);
-$scripts = str_ireplace('http://live.wallstreetcn.com', 'http://img.wallstreetcn.com', $scripts);
 ?>
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
--- sites/all/themes/wallstcnlive/templates/html.tpl.php
+++ sites/all/themes/wallstcnlive/templates/html.tpl.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+<?
+$styles = str_ireplace('http://live.wallstreetcn.com', 'http://img.wallstreetcn.com', $styles);
+$scripts = str_ireplace('http://live.wallstreetcn.com', 'http://img.wallstreetcn.com', $scripts);
+?>
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
