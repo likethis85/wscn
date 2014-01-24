8497be23e32e16e50328314cb98711566442028c
han1015578159 <77620429@qq.com>
2014-01-24 11:17:44 +0800

    ads

--- sites/all/themes/wallstcn/templates/region--index_content.tpl.php
+++ sites/all/themes/wallstcn/templates/region--index_content.tpl.php
@@ -18,15 +18,19 @@
         <div class="span4">
             <div class="random-ad">
                 <div class="carousel-inner">
+                    <!--
                     <div class="active item" data-probability="33">
                         <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" width="234" height="60"><param name="quality" value="high" /><param name="movie" value="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/__ads_wscn_index_hantec_1.swf" /><embed pluginspage="http://www.macromedia.com/go/getflashplayer" quality="high" src="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/__ads_wscn_index_hantec_1.swf" type="application/x-shockwave-flash" width="234" height="60"></embed></object>
                     </div>
-                    <div class="item" data-probability="33">
+                    -->
+                    <div class="activeitem" data-probability="100">
                         <a target="_blank" href="redirect.htm?type=__ads_wscn_index_hit_forexct_1&url=http://www.forexct.com/pr/fcity/forexct.htm?SerialId=1066233"><img src="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/__ads_wscn_index_forexct_1.jpg" alt=""></a>
                     </div>
+                    <!--
                     <div class="item">
                         <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" width="234" height="60"><param name="quality" value="high" /><param name="movie" value="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/__ads_wscn_index_svsfx_2.swf" /><embed pluginspage="http://www.macromedia.com/go/getflashplayer" quality="high" src="http://img.wallstreetcn.com/sites/all/themes/wallstcn/ads/__ads_wscn_index_svsfx_2.swf" type="application/x-shockwave-flash" width="234" height="60"></embed></object>
                     </div>
+                    -->
                 </div>
             </div>
         </div>
