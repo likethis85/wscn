7bca56f7c01968ae9359583c7f145d1f15c55de6
han1015578159 <77620429@qq.com>
2014-01-27 15:53:50 +0800

    feedback

--- feedback/fb.php
+++ feedback/fb.php
@@ -1,7 +1,9 @@
 <?php
 $conn = mysql_connect('10.4.6.164', 'root', 'huaerjie001');
-
+//$conn = mysql_connect('localhost', 'root', 'admin');
 mysql_select_db("wscn", $conn);
+
+mysql_query("SET NAMES 'UTF8'");
 
 $title   = htmlspecialchars(trim($_POST['title']));
 $message = htmlspecialchars(trim($_POST['message']));
